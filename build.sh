130b8d94c91fb1cb9283f7eb4f130c374597dcec
Hetsh <git@hetsh.de>
2020-08-25 13:08:41 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,38 +21,45 @@ fi
 
 # Build the image
 APP_NAME="umurmur"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	# Set up temporary directory
-	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DIR"
-	echo "channels = (
+case "${1-}" in
+	"--test")
+		# Set up temporary directory
+		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
+		add_cleanup "rm -rf $TMP_DIR"
+		echo "channels = (
 	{
 		name = \"Test\";
 		parent = \"\";
 	}
 );" > "$TMP_DIR/umurmur.conf"
 
-	# Apply permissions, UID & GID matches process user
-	extract_var APP_UID "./Dockerfile" "\K\d+"
-	extract_var APP_GID "./Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_GID" "$TMP_DIR"
+		# Apply permissions, UID & GID matches process user
+		extract_var APP_UID "./Dockerfile" "\K\d+"
+		extract_var APP_GID "./Dockerfile" "\K\d+"
+		chown -R "$APP_UID":"$APP_GID" "$TMP_DIR"
 
-	# Start the test
-	extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
-	extract_var CONF_FILE "./Dockerfile" "\"\K[^\"]+"
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 64738:64738/tcp \
-	--publish 64738:64738/udp \
-	--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
-	--mount type=bind,source="$TMP_DIR/umurmur.conf",target="$CONF_FILE" \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_NAME"
-fi
+		# Start the test
+		extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
+		extract_var CONF_FILE "./Dockerfile" "\"\K[^\"]+"
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 64738:64738/tcp \
+		--publish 64738:64738/udp \
+		--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
+		--mount type=bind,source="$TMP_DIR/umurmur.conf",target="$CONF_FILE" \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
